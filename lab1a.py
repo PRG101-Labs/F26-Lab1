b91f5a34f8c5208ab88dab1cc082827d40a504a7
BushraBashirS <bushra.bashir25@gmail.com>
2026-09-09 11:50:01 -0400

    Fix usage instruction typo in lab1a.py
    Corrected the usage instruction in the comments.

--- lab1a.py
+++ lab1a.py
@@ -4,7 +4,7 @@
 # Author:
 # Date:
 # Purpose: Create a variable, check its type and print the variable.
-# Usage: ./lab1a.py
+# Usage: pthon3 lab1a.py
 
 # TO DO 1: Creating and using varibales
 # create a variable called message.
